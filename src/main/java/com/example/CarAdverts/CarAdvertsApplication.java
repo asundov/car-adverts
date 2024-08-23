ba6c8c84bd688411e90517c6359c8dd6e51a9b66
tanasun <ana.sundov.milicic@ericssonnikolatesla.com>
2024-08-23 17:48:05 +0200

    init

---
+++ src/main/java/com/example/CarAdverts/CarAdvertsApplication.java
@@ -0,0 +1,13 @@
+package com.example.CarAdverts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarAdvertsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarAdvertsApplication.class, args);
+	}
+
+}
